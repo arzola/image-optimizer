b0709aebacc659d101107bf4523d05e749103545
Oscar Arzola <oscararzola@gmail.com>
2016-12-19 18:10:27 -0600

    Init repo

---
+++ config/imageoptimizer.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /*
+     |--------------------------------------------------------------------------
+     | Options for image transforming
+     |--------------------------------------------------------------------------
+     |
+     | Bin path you can check easy with follow command in a shell:
+     | which optipng
+     |
+     */
+    'options' => [
+        'ignore_errors' => false,
+
+        'pngquant_bin'     => '/usr/local/bin/pngquant',
+        'pngquant_options' => ['--force'],
+
+        'gifsicle_bin'     => '/usr/local/bin/gifsicle',
+        'gifsicle_options' => ['-b', '-O5'],
+
+        'jpegoptim_bin'     => '/usr/local/bin/jpegoptim',
+        'jpegoptim_options' => ['--strip-all'],
+
+    ],
+
+
+    /*
+     |--------------------------------------------------------------------------
+     | Transformer for image
+     |--------------------------------------------------------------------------
+     |
+     | You can choice which tranformer you will use
+     |
+     */
+    'transform_handler' => [
+        'png'  => 'pngquant',
+        'jpg'  => 'jpegoptim',
+        'jpeg' => 'jpegoptim',
+        'gif'  => 'gifsicle',
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Log file
+     |--------------------------------------------------------------------------
+     |
+     | Only for image optimize errors
+     |
+     */
+    'log_file' => storage_path().'/logs/image_optimize.log',
+
+];
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Image Optimizer API powered by Laravel
+### Google Page speed frienfly
+git 
+This laravel app provide a sweet API to optimize images with a simple http call.
+
+This package use the awesome wrapper [Laravel Imageoptimizer](https://github.com/approached/laravel-image-optimizer) to handle image transformers.
+
+Features
+
+- API interface
+- You can host on your server
+- Laravel Queue Jobs
