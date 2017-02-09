335ca03f7af2e66e8cf9c7a62fc64215fa74b646
Oscar Arzola <oscararzola@gmail.com>
2017-02-09 17:47:02 -0600

    Fixed upsizing and test cases
    * Updates icode
    
    * Temporary workaround to avoid api tokenization
    
    * Fixed issue
    
    * Fixed upsized images

--- app/Http/Controllers/Optimizer.php
+++ app/Http/Controllers/Optimizer.php
@@ -26,8 +26,11 @@ class Optimizer extends Controller
         $img = Image::make(storage_path('app/compressed/') . $newName);
         $img->resize($width, $height, function ($constraint) {
             $constraint->aspectRatio();
+            $constraint->upsize();
         });
         $img->save(storage_path('app/resized/') . $newName);
+        unlink(storage_path('app/compressed/') . $newName);
+        return $img->response();
     }
 
     public function resizeremote(Request $request, ImageOptimizer $imageOptimizer)
@@ -44,6 +47,7 @@ class Optimizer extends Controller
         $img = Image::make($file);
         $img->resize($width, $height, function ($constraint) {
             $constraint->aspectRatio();
+            $constraint->upsize();
         });
         return $img->response();
     }
